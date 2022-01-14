79e4d494b4aeea4408d5c9366c8849a681d31b8f
Zachary Halperin <zacharykhalperin@gmail.com>
2022-01-14 01:39:48 -0700

    first commit

--- projects/project-1/tests.js
+++ projects/project-1/tests.js
@@ -19,7 +19,7 @@ window.onload = () => {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(showPosition);
         } else {
-            x.innerHTML += "Geolocation is not supported by this browser.";
+            x.innerHTML += "Geolocation isnot supported by this browser.";
         }
     }
     let showPosition = (position) => {
