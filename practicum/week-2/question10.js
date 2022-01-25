4c4dad959d8eabc263100b8cddf5445beec3ff0a
Zachary Halperin <zacharykhalperin@gmail.com>
2022-01-25 15:59:27 -0700

    Q10

--- practicum/week-2/question10.js
+++ practicum/week-2/question10.js
@@ -8,5 +8,11 @@ output: 7 - 2 = 5
 */
 
 var maxMinusMin = function(n) {
-
+    var n = String(n);
+    var arr = [];
+    for(var i=0; i<n.length; i++){
+        arr.push(parseInt(n[i]));
+    }
+    arr = arr.sort()
+    return arr[arr.length-1] - arr[0];
 };
