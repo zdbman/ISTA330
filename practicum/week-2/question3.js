fa98c11249aebd6c7205e8ee730444020dccabd8
Zachary Halperin <zacharykhalperin@gmail.com>
2022-01-24 17:19:29 -0700

    Q3

--- practicum/week-2/question3.js
+++ practicum/week-2/question3.js
@@ -9,5 +9,13 @@ There are 5 ordered pairs: (4, 6), (4, 9), (2, 6), (2, 9), (6, 9)
 */
 
 var numberOfOrderedPairs = function(input) {
-
+    var sum = 0;
+    for(var i=0; i<input.length; i++){
+        for(var j=i; j<input.length; j++){
+            if(input[i] < input[j]){
+                sum += 1;
+            }
+        }
+    }
+    return sum;
 };
