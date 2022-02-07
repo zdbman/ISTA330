3c5e775df9ab273f458041324eff20f56329f941
Zachary Halperin <zacharykhalperin@gmail.com>
2022-02-07 02:51:38 -0700

    Q10

--- practicum/week-3/question10.js
+++ practicum/week-3/question10.js
@@ -16,5 +16,30 @@ output: [[-5, -4], [23, 24]]
 */
 
 var minPairs = function(input) {
-
+    var min = input[0] - input[1];
+    var count = 0;
+    var lst = [];
+    for(var i=0; i<input.length; i++){
+        for(var j=i+1; j<input.length; j++){
+            if(Math.abs(input[i]-input[j]) <= min){
+                min = Math.abs(input[i]-input[j]);
+                lst[count] = [input[i], input[j]];
+                count += 1;
+            }
+            if(Math.abs(input[j]-input[i]) <= min){
+                min = Math.abs(input[j]-input[i]);
+                lst[count] = [input[j], input[i]];
+                count += 1;
+            }
+        }
+    }
+    var retlst = [];
+    var count = 0
+    for(var i=0; i<lst.length; i++){
+        if(Math.abs(lst[i][0] - lst[i][1]) == min && lst[i][0] < lst[i][1]){
+            retlst[count] = lst[i];
+            count++;
+        }
+    }
+    return retlst;
 };
