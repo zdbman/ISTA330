1266479ff2de33b1c1a5e6b9ade37d8b7372ea3a
Zachary Halperin <zacharykhalperin@gmail.com>
2022-02-17 14:44:12 -0700

    Q9

--- practicum/week-3/question9.js
+++ practicum/week-3/question9.js
@@ -13,5 +13,38 @@ so there are two groups with the largest size.
 */
 
 var largestGroupsCount = function(n) {
-
+    var count = 0;
+    //console.log(n.toString());
+    var lst = [];
+    var c = 0;
+    for(var i=1; i<=n; i++){
+        for(var j=10; j<=n; j++){
+            var test = j.toString();
+            //console.log(test);
+            var num = 0;
+            for(var k=0; k<test.length; k++){
+                num += parseInt(test[k]);
+            }
+            if(num === i){
+                //console.log(i, num);
+                count += 1;
+                lst[c] = i;
+                c++;
+            }
+        }
+    }
+    console.log(lst)
+    var c4 = 0;
+    for(var i=0; i<lst.length; i++){
+        var t = true;
+        for(var j=i+1; j<lst.length; j++){
+            if(lst[i] == lst[j]){
+                t = false;
+            }
+        }
+        if(t){
+            c4+=1;
+        }
+    }
+    return c4;
 };
